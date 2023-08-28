4c9fe68ffddc730dd2d831c3b2eb8237a75965f6
Dineshmw <dineshweerasekara1@gmail.com>
2023-08-28 11:55:25 +0530

    Psycopg2 instalation with dependencies

---
+++ app/app/calc.py
@@ -0,0 +1,17 @@
+'''
+calculator functions
+'''
+
+
+def add(x,y):
+    """Add x and y and return the result"""
+    return x + y
+
+def subtract(x,y):
+    """Subtract y from y and return the result"""
+    return x - y
+
+
+
+
+
