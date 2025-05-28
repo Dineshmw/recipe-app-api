88b9f308d4b1d0fece61945e8e5fbb494a5269b2
Dineshmw <dineshwerasekara1@gmail.com>
2025-05-28 16:27:46 +0530

    Test configuration | Database service configuration

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculation function
+"""
+
+
+def add(x, y):
+    """Add x and y and return the result"""
+    return x + y
+
+
+def subtract(x, y):
+    """Substract numbers and return results"""
+    return y - x
