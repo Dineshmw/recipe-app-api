63cd47e832fa44900c3acac58e0a7053051dfbdb
Dineshmw <dineshwerasekara1@gmail.com>
2025-06-08 15:37:57 +0530

    Authetication configured, User API completed

--- app/app/urls.py
+++ app/app/urls.py
@@ -18,7 +18,7 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
     )
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 
 urlpatterns = [
@@ -27,5 +27,6 @@ urlpatterns = [
     path(
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema')
-    )
+    ),
+    path('api/user/', include('user.urls'))
 ]
